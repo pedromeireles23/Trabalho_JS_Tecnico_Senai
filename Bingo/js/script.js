38b7984a0d4d076150f03ed5cdf7505415d23187
pedromeireles23 <pemeirels@gmail.com>
2021-05-20 15:30:24 -0300

    Toques finais e completando racíociono do bingo

--- Bingo/js/script.js
+++ Bingo/js/script.js
@@ -1,19 +1,35 @@
- var test = document.getElementsByTagName('td')
- let pedras = []
- let cartela = []
-
-const randomunique = (range,count) => {
-    let nums = new Set();
-    while (nums.size<count){
-        nums.add(Math.flor(Math.random()*(range-1+1)+1))
-    }
-    return {...nums}
-}
-for(let i =1;i<=60;i++){
+let cartela = [];
+let pedras = [];
+let colunasTabela = document.getElementsByTagName("td");
+for (let i = 1; i <= 60; i++) {
     pedras.push(i)
 }
+function gerarCartela() {
+    event.preventDefault();
+    const randomUnique = (range, count) => {
+        let nums = new Set();
+        while (nums.size < count) {
+            nums.add(Math.floor(Math.random() * (range - 1 + 1) + 1));
+        }
+        return [...nums];
+    }
 
-cartela= randomunique(pedras.length, test.length)
-for(let ){
-    
+
+
+    cartela = randomUnique(pedras.length, colunasTabela.length);
+    for (let i = 0; i < colunasTabela.length; i++) {
+        colunasTabela[i].textContent = cartela[i];
+    }
+
 }
+
+
+function sortearPedra() {
+    var pedraSelecionada = pedras.find((item) => item === Math.floor(Math.random() * pedras.length))
+    if (pedraSelecionada != undefined) {
+        alert(`A pedra selecionada é: ${pedraSelecionada}`);
+        let indexItemCartela = cartela.findIndex(item => item === pedraSelecionada)
+        colunasTabela[indexItemCartela].style.background = "pink"
+    }
+}
+
